367dc0f3061ed84726911adcaf1f50933a321297
Matteo Magnini <matteo.magnini@studio.unibo.it>
2022-02-17 16:58:46 +0100

    bug fix in import

--- psyki/fol/__init__.py
+++ psyki/fol/__init__.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Set, List, TypeVar
 from tuprolog.core import Var, Term
-
-from resources.dist.resources.FOLVisitor import FOLVisitor
 
 T = TypeVar('T')
 
