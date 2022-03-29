ea132a5c03c27f7665cdc88e51104c0838e26a51
Matteo Magnini <matteo.magnini@studio.unibo.it>
2022-03-29 14:45:12 +0200

    fix: add setup

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def get_version_from_git():
         if version_file.exists():
             return version_file.read_text().strip()
         else:
-            return '0.1.0.archeo'
+            return '0.1.0'
 
 
 version = get_version_from_git()
