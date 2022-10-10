bcab9b1e8173933371af81be149c9af0bd42449c
AndAgio <aagiollo@gmail.com>
2022-10-10 16:18:27 +0200

    WIP - utilities for multiple metrics

--- psyki/qos/latency.py
+++ psyki/qos/latency.py
@@ -82,11 +82,3 @@ def measure_predict(model: Union[Model, EnrichedModel],
     # Stop the timer to get timings information
     end = time.time()
     return end - start
-
-
-def split_dataset(dataset: Dataset) -> List[tuples]:
-    # Split dataset into train and test
-    train, _ = train_test_split(dataset, test_size=0.5, random_state=0)
-    train_x, train_y = train.iloc[:, :-1], train.iloc[:, -1]
-    test_x, test_y = test.iloc[:, :-1], test.iloc[:, -1]
-    return train_x, train_y, test_x, test_y
