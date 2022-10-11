adb979e3d24ad14ca747e55deac49d6163ea000b
AndAgio <aagiollo@gmail.com>
2022-10-11 15:19:31 +0200

    WIP - testing metrics

---
+++ test/psyki/qos/latency.py
@@ -0,0 +1,27 @@
+import unittest
+import pandas as pd
+from test.resources.data import get_dataset
+from test.utils import create_standard_fully_connected_nn
+from test.resources.rules import get_rules, FEATURE_MAPPING as POKER_FEATURE_MAPPING, CLASS_MAPPING as POKER_CLASS_MAPPING
+from psyki.ski import Injector, Formula
+from psyki.qos import LatencyQoS
+from psyki.logic.datalog.grammar.adapters import antlr4
+
+
+class TestLatency(unittest.TestCase):
+    data = pd.DataFrame(get_dataset('train'), dtype='int32')
+    model = create_standard_fully_connected_nn(10, 10, 3, 128, 'relu')
+    injector = 'kill'
+    injector_arguments = {'class_mapping': POKER_CLASS_MAPPING,
+                          'feature_mapping': POKER_FEATURE_MAPPING}
+    formulae = [antlr4.get_formula_from_string(rule) for rule in get_rules()]
+
+    def measure_fit(self):
+        options = {'optim': 'adam',
+                   'loss': 'sparse_categorical_crossentropy',
+                   'batch': 32,
+                   'epochs': 2,
+                   'dataset': data,
+                   'formula': formulae}
+        qos = LatencyQoS(self.model, self.injector, self.injector_arguments, self.formulae, options)
+        qos.measure(fit=True)
---
+++ test/psyki/qos/memory.py
@@ -0,0 +1,19 @@
+import unittest
+import pandas as pd
+from test.resources.data import get_dataset
+from test.utils import create_standard_fully_connected_nn
+from test.resources.rules import get_rules, FEATURE_MAPPING as POKER_FEATURE_MAPPING, CLASS_MAPPING as POKER_CLASS_MAPPING
+from psyki.ski import Injector, Formula
+from psyki.qos import MemoryQoS
+from psyki.logic.datalog.grammar.adapters import antlr4
+
+
+class TestMemory(unittest.TestCase):
+    data = pd.DataFrame(get_dataset('train'), dtype='int32')
+    model = create_standard_fully_connected_nn(10, 10, 3, 128, 'relu')
+    injector = Injector.kill(model, POKER_CLASS_MAPPING, POKER_FEATURE_MAPPING)
+    formulae = [antlr4.get_formula_from_string(rule) for rule in get_rules()]
+
+    def measure(self):
+        qos = MemoryQoS(self.model, self.injector)
+        qos.measure(mode='flops')
