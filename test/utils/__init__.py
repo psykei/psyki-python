13fbd98e74c63386ce039f09574772b67bff2bc7
AndAgio <aagiollo@gmail.com>
2022-10-11 15:21:11 +0200

    WIP - help function to create fc nn

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -26,6 +26,15 @@ def get_mlp(input_layer: Tensor, output: int, layers: int, neurons: int or list[
     return Dense(output, activation=last_activation_function)(x)
 
 
+def create_standard_fully_connected_nn(input_size: int, output_size, layers: int, neurons: int, activation: str) -> Model:
+    inputs = Input((input_size,))
+    x = Dense(neurons, activation=activation)(inputs)
+    for _ in range(1, layers):
+        x = Dense(neurons, activation=activation)(x)
+    x = Dense(output_size, activation='softmax' if output_size > 1 else 'sigmoid')(x)
+    return Model(inputs, x)
+
+
 def get_processed_dataset(name: str, validation: float = 1.0):
     training = get_dataset(name, 'train')
     testing = get_dataset(name, 'test')
