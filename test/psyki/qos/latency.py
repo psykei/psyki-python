ef88749cdad5c8e0eb8154f008865f7a2452e10e
andrearafanelli <andrea.rafanelli96@gmail.com>
2022-10-11 18:44:32 +0200

    Update testing Latency metric

--- test/psyki/qos/latency.py
+++ test/psyki/qos/latency.py
@@ -9,19 +9,20 @@ from psyki.logic.datalog.grammar.adapters import antlr4
 
 
 class TestLatency(unittest.TestCase):
-    data = pd.DataFrame(get_dataset('train'), dtype='int32')
-    model = create_standard_fully_connected_nn(10, 10, 3, 128, 'relu')
-    injector = 'kill'
-    injector_arguments = {'class_mapping': POKER_CLASS_MAPPING,
-                          'feature_mapping': POKER_FEATURE_MAPPING}
-    formulae = [antlr4.get_formula_from_string(rule) for rule in get_rules()]
+    def __init__(self):
+        self.data = pd.DataFrame(get_dataset('train'), dtype = 'int32')
+        self.model = create_standard_fully_connected_nn(10, 10, 3, 128, 'relu')
+        self.injector = 'kill'
+        self.injector_arguments = {'class_mapping': POKER_CLASS_MAPPING,
+                           'feature_mapping': POKER_FEATURE_MAPPING}
+        self.formulae = [antlr4.get_formula_from_string(rule) for rule in get_rules()]
 
     def measure_fit(self):
         options = {'optim': 'adam',
-                   'loss': 'sparse_categorical_crossentropy',
-                   'batch': 32,
-                   'epochs': 2,
-                   'dataset': data,
-                   'formula': formulae}
+                    'loss': 'sparse_categorical_crossentropy',
+                    'batch': 32,
+                    'epochs': 2,
+                    'dataset': self.data,
+                    'formula': self.formulae}
         qos = LatencyQoS(self.model, self.injector, self.injector_arguments, self.formulae, options)
-        qos.measure(fit=True)
+        qos.measure(fit = True)
