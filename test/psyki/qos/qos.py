b4e5c2b77fadd0234d3f7b2725acde7b0ac8ce4e
AndAgio <aagiollo@gmail.com>
2022-11-09 11:41:46 +0100

    test(qos): Testing QoS with and without grid search.

--- test/psyki/qos/qos.py
+++ test/psyki/qos/qos.py
@@ -17,19 +17,19 @@ class TestQoS(unittest.TestCase):
                                                layers=3,
                                                neurons=128,
                                                activation='relu')
-    injector = 'kill'
+    injector = 'kins'
     class_mapping = {'setosa': 0, 'virginica': 1, 'versicolor': 2}
     variable_mapping = {'SL': 0, 'SW': 1, 'PL': 2, 'PW': 3}
-    injector_arguments = {'class_mapping': class_mapping,
-                          'feature_mapping': variable_mapping}
+    injector_arguments = {#'class_mapping': class_mapping,
+                          'feature_mapping': variable_mapping,
+                            'injection_layer': len(model.layers) - 2}
     formulae = [antlr4.get_formula_from_string(rule) for rule in get_rules('iris')]
 
     def test_qos(self):
-        metric_arguments = dict(injection=self.injector,
+        metric_arguments = dict(model=self.model,
+                                injection=self.injector,
                                 injector_arguments=self.injector_arguments,
                                 formulae=self.formulae,
-                                max_neurons=[1000, 1000],
-                                grid_levels=100,
                                 optim='adam',
                                 loss='sparse_categorical_crossentropy',
                                 batch=16,
@@ -38,10 +38,19 @@ class TestQoS(unittest.TestCase):
                                 dataset=self.dataset,
                                 threshold=0.97,
                                 alpha=0.8)
-        flags = dict(energy=True,
-                     latency=True,
+        flags = dict(energy=False,
+                     latency=False,
                      memory=True,
-                     grid_search=True)
+                     grid_search=False)
+
+        qos = QoS(metric_arguments=metric_arguments,
+                  flags=flags)
+        qos.compute(verbose=False)
+
+        metric_arguments['max_neurons'] = [1000, 1000]
+        metric_arguments['grid_levels'] = 100
+        metric_arguments['injector_arguments']['injection_layer'] = len(metric_arguments['max_neurons'])
+        flags['grid_search'] = True
 
         qos = QoS(metric_arguments=metric_arguments,
                   flags=flags)
