dad964d0b0171a815aa794b4dc5954d335a98aa4
Matteo Magnini <matteo.magnini@studio.unibo.it>
2023-05-26 14:21:31 +0200

    feat: log info on predictor evaluation

--- test/psyki/ski/__init__.py
+++ test/psyki/ski/__init__.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from sklearn.model_selection import train_test_split
 from tensorflow.python.framework.random_seed import set_seed
 import psyki
 import pandas as pd
@@ -11,6 +12,7 @@ from tensorflow.python.keras.utils.np_utils import to_categorical
 
 
 SEED = 0  # Set seed to make sure that the test is reproducible
+TEST_SIZE = 1/3  # 1/3 is a fairly common test size
 EPOCHS = 2  # Just check if it runs without errors (2 epochs are required because only one could not catch some errors)
 BATCH_SIZE = 32  # 32 is a fairly common batch size
 VERBOSE = 0  # 0 = silent, 1 = progress bar, 2 = one line per epoch
@@ -31,11 +33,15 @@ class TestInjector(unittest.TestCase):
         psyki.logger.info(f"testing educated training")
         time = datetime.now()
         set_seed(SEED)
-        train_x, train_y = dataset.iloc[:, :-1], to_categorical(dataset.iloc[:, -1:])
+        train, test = train_test_split(dataset, test_size=TEST_SIZE, random_state=SEED, stratify=dataset.iloc[:, -1:])
+        train_x, train_y = train.iloc[:, :-1], to_categorical(train.iloc[:, -1:])
+        test_x, test_y = test.iloc[:, :-1], to_categorical(test.iloc[:, -1:])
         history = educated.fit(
             train_x, train_y, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE
         )
         self.assertEqual(len(history.history["loss"]), EPOCHS)
+        loss, acc = educated.evaluate(test_x, test_y, verbose=VERBOSE)
+        psyki.logger.info(f"accuracy: {acc}")
         psyki.logger.info(f"test ended in {datetime.now() - time}")
 
     def _test_educated_is_cloneable(self, educated: Model):
